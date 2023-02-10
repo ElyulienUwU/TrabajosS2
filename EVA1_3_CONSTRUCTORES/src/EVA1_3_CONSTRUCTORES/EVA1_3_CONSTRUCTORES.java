058c9ed08353acdb11c056a6d30f7f01d20bbe2e
ElyulienUwU <124391764+ElyulienUwU@users.noreply.github.com>
2023-02-10 15:16:11 -0600

    Add files via upload

---
+++ EVA1_3_CONSTRUCTORES/src/EVA1_3_CONSTRUCTORES/EVA1_3_CONSTRUCTORES.java
@@ -0,0 +1,25 @@
+//Julian Fernando Ortiz Lerma
+//22550335
+//Programacion Orientada a Objetos
+package EVA1_3_CONSTRUCTORES;
+
+public class EVA1_3_CONSTRUCTORES {
+
+	public EVA1_3_CONSTRUCTORES() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CuentaBancaria c1 = new CuentaBancaria();
+		c1.setCliente("Julian Ortiz");
+		c1.setCuenta("123");
+		c1.setSaldo(5000);
+		
+		c1.Datos();
+		
+		
+}
+	
+}
